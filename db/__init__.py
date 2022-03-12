53cae72d595b49b9bf3919bbd14c9cbf45e96e86
dpalmasan <dpalmasan@gmail.com>
2022-03-12 19:23:07 -0300

    Add logs to make debugging easier (#14)

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,31 @@
 from config import Config
 from db.session import create_db_session
+from pathlib import Path
+import logging
+import os
 
-config = Config.load_from_file("app_config/config.yaml")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+# create formatter
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
+cwd = os.getcwd()
+logger.info(f"Working directory {cwd}")
+logger.info(f"Files {os.listdir(cwd)}")
+config_file = Path("app_config/config.yaml")
+if not config_file.exists():
+    logger.info("No app_config/config.yaml was found ")
+    config_file = Path("app_config/config.default.yaml")
+
+logger.info(f"Using {config_file}")
+config = Config.load_from_file(config_file)
 session = create_db_session(config)
--- db/session.py
+++ db/session.py
@@ -1,7 +1,11 @@
 from config import Config
 from pymongo import MongoClient
+import logging
 
 
 def create_db_session(config: Config) -> MongoClient:
     client = MongoClient(host=config.db_uri, port=config.db_port)
+    logging.info(
+        f"Creating Mongo client host: {config.db_uri} port: {config.db_port}"
+    )
     return client[config.db_name]
